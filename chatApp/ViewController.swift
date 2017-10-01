547c76b6798d3a5ca8d00800c645e9d65b9eb22c
nschmidt8281 <27870681+nschmidt8281@users.noreply.github.com>
2017-10-01 16:24:14 -0500

    Initial Commit

---
+++ chatApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chatApp
+//
+//  Created by Nicolas Schmidt on 10/1/17.
+//  Copyright © 2017 408 Industries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
